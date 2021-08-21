9030ae36195aa8a36acb10c208fe4fc8c23ea118
Kiran Bodipati <kiranbodipati@gmail.com>
2021-08-21 14:59:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-# from flask import Flask
-# from flask_mongoengine import MongoEngine
-# import db
+from flask_mongoengine import MongoEngine
+import db
 from pipeline import *
 from optical_flow_spell_corrector import *
+from flask import Flask, request, url_for, jsonify
+from flask_pymongo import PyMongo
+from pymongo.errors import DuplicateKeyError
+from models import *
+import json
 
-# app = Flask(__name__)
-# @app.route('/')
-# def hello():
-#     return "Hello World! I am Kiran"
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hello World! I am Kiran"
 
-# #test to insert data to the data base
-# @app.route("/test")
-# def test():
-#     db.db.collection.insert_one({"name": "John"})
-#     return "Connected to the data base!"
+#test to insert data to the data base
+@app.route("/test")
+def test():
+    db.db.person.insert_one({"name": "John", 'LinkedinURL':"https://www.linkedin.com/in/kiranbodipati/"})
+    return "Connected to the data base!"
 
 
-def run_extraction_pipeline():
-    imagearray = uniqueFrames(videoUrl="./videos/TheLastofUs2Credits_Trim.mp4", isYoutubeUrl=False)
+def run_extraction_pipeline(video="./videos/TheLastofUs2Credits_Trim.mp4", yt=False):
+    imagearray = uniqueFrames(videoUrl=video, isYoutubeUrl=yt)
     print(len(imagearray))
     # person_names = []
     # person_names_pos = []
@@ -48,6 +52,123 @@ def run_extraction_pipeline():
                 overall_jobs_list.update(final_job_titles_list)
     print(overall_jobs_list)
 
-# if __name__ == "__main__":
-#   app.run(debug=True)
-run_extraction_pipeline()
+@app.route("/database/persons", methods=['GET', 'POST'])
+def person_read_write():
+    if request.method=='POST':
+        raw_person= request.get_json()
+        # raw_person =jsonify(raw_person)
+        if db.db.person.find_one(raw_person):
+            return("Person Exists")
+        else:
+            person=Person(**raw_person)
+            # print(raw_person)
+            db.db.person.insert_one(person.to_bson())
+            print("person added")
+            return "person added"
+    
+    if request.method=='GET':
+        persons_cursor = db.db.person.find({})
+        person_list=[]
+        for doc in persons_cursor:
+            person=Person(**doc)
+            person = person.to_json()
+            person_list.append(person)
+        return jsonify(person_list)
+
+@app.route("/database/person", methods=['POST', 'DELETE'])
+def get_delete_one_person():
+    param = request.get_json()
+    person = db.db.person.find_one(param)
+    if person:
+        if request.method=='POST':
+            person=Person(**person)
+            return person.to_json()
+        if request.method=='DELETE':
+            db.db.person.find_one_and_delete(param)
+        person=Person(**person)
+        return person.to_json()
+    else:
+        return("person does not exist")
+
+
+
+
+@app.route("/database/games", methods=['GET', 'POST'])
+def game_read_write():
+    if request.method=='POST':
+        raw_game= request.get_json()
+        if db.db.game.find_one(raw_game):
+            return("Game Exists")
+        else:
+            game=Game(**raw_game)
+            db.db.game.insert_one(game.to_bson())
+            print("game added")
+            return "game added"
+    
+    if request.method=='GET':
+        game_cursor = db.db.game.find({})
+        game_list=[]
+        for doc in game_cursor:
+            game=Game(**doc)
+            game = game.to_json()
+            game_list.append(game)
+        return jsonify(game_list)
+
+
+@app.route("/database/game", methods=['POST', 'DELETE'])
+def get_delete_one_game():
+    param = request.get_json()
+    game = db.db.game.find_one(param)
+    if game:
+        if request.method=='POST':
+            game=Game(**game)
+            return game.to_json()
+        if request.method=='DELETE':
+            db.db.game.find_one_and_delete(param)
+        game=Game(**game)
+        return game.to_json()
+    else:
+        return("game does not exist")
+
+
+@app.route("/database/worksAt")
+def covert_to_person_list(job_list):
+    overall_person_list={}
+    for job in job_list.keys():
+        person_list = job_list[job]
+        for person in person_list:
+            if person not in overall_person_list.keys():
+                overall_person_list[person] =[job]
+            else:
+                overall_person_list[person].append(job)
+    personJsonList={}
+
+@app.route("/database/getJobs", methods=['GET', 'POST'])
+def get_job_list():
+    if request.method=='GET':
+        job_cursor = db.db.worksAt.find({})
+        job_list=[]
+        for doc in job_cursor:
+            job=WorksAt(**doc)
+            job = job.to_json()
+            job_list.append(job)
+        return jsonify(job_list)
+    
+    if request.method=='POST':
+        raw_jobs= request.get_json()
+        # raw_person =jsonify(raw_person)
+        if db.db.worksAt.find_one(raw_jobs):
+            return("data Exists")
+        else:
+            person=WorksAt(**raw_jobs)
+            # print(raw_person)
+            db.db.worksAt.insert_one(person.to_bson())
+            print("data added")
+            return "data added"
+
+job_list={"Written By":['NEIL DRUCKMANN', 'HALLEY GROSS'], "Additional Writing":['JOSH SCHERR', 'RYAN JAMES'], "President":['NEIL DRUCKMANN']}
+
+# covert_to_person_list(job_list)
+if __name__ == "__main__":
+   app.run(debug=True)
+# # run_extraction_pipeline()
--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ import random
 import argparse
 import requests
 import re
-
+from fake_useragent import UserAgent
+import time
 
 class LinkedinScraper(object):
     def __init__(self, keyword, limit):
@@ -22,16 +23,24 @@ class LinkedinScraper(object):
     
     
         # choose a random user agent
-        user_agents = [
-            'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36',
-            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0) chromeframe/10.0.648.205',
-            'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6',
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19 (KHTML, like Gecko) Ubuntu/11.10 Chromium/18.0.1025.142 Chrome/18.0.1025.142 Safari/535.19',
-            'Mozilla/5.0 (Windows NT 5.1; U; de; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 Opera 11.00'
-        ]
+        try:
+            ua = UserAgent()
+        except FakeUserAgentError:
+            ua=None
+            user_agents = [
+                'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36',
+                'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0) chromeframe/10.0.648.205',
+                'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36',
+                'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6',
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19 (KHTML, like Gecko) Ubuntu/11.10 Chromium/18.0.1025.142 Chrome/18.0.1025.142 Safari/535.19',
+                'Mozilla/5.0 (Windows NT 5.1; U; de; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 Opera 11.00'
+            ]
         while self.counter < self.limit:
-            headers = {'User-Agent': random.choice(user_agents)}
+            if ua==None:
+                headers = {'User-Agent': random.choice(user_agents)}
+            else:
+                headers = {'User-Agent': ua.random}
+            time.sleep(2)
             url = 'http://google.com/search?num=100&start=' + str(self.counter) + '&hl=en&meta=&q=site%3Alinkedin.com/in%20' + self.keyword
             resp = requests.get(url, headers=headers)
             if ("Our systems have detected unusual traffic from your computer network.") in resp.text:
@@ -39,7 +48,7 @@ class LinkedinScraper(object):
                 return
         
             self.all_htmls += resp.text
-            self.counter += 10
+            self.counter += 5
 
     def parse_links(self):
         reg_links = re.compile(r"url=https:\/\/*[a-z]+\.linkedin.com(.*?)&")
@@ -76,12 +85,12 @@ class LinkedinScraper(object):
     
 
     
-# ls = LinkedinScraper(keyword="Written By",limit=5)
+# ls = LinkedinScraper(keyword="Kiran Bodipati",limit=5)
 # ls.search()
 # links = ls.parse_links()
 # profiles = ls.parse_people()
 
-
+#print(links)
 
 # print(stripped)
 # driver = webdriver.Chrome("D:/chromedriver.exe")
