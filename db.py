f46d1f1cf4119e9bc6e93b9d0392332bd81a029d
Kiran Bodipati <kiranbodipati@gmail.com>
2021-10-01 22:48:25 +0800

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import pymongo
 from main import app
 
-client = pymongo.MongoClient("mongodb+srv://Kiran:kiran$2000@cluster0.mfqoj.mongodb.net/edge-backend?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://<Username>:<Password>@cluster0.mfqoj.mongodb.net/edge-backend?retryWrites=true&w=majority")
 
 db = client.get_database('flask_mongodb_atlas')
 user_collection = pymongo.collection.Collection(db, 'user_collection')
